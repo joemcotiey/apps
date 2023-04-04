53ebe1efe72a47cef84893441f8f71c3471e1234
joemcotiey <joemcotiey@gmail>
2023-04-04 13:23:00 +0300

    first commit

---
+++ ap23/bin/ap23.dart
@@ -0,0 +1,5 @@
+import 'package:ap23/ap23.dart' as ap23;
+
+void main(List<String> arguments) {
+  print('Joe says hi');
+}
---
+++ ap23/test/ap23_test.dart
@@ -0,0 +1,8 @@
+import 'package:ap23/ap23.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
